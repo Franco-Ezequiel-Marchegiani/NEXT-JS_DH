58bf31e344131023adc5cf92e135c2bb51ea38d3
Franco Ezequiel <eze.cavs@hotmail.com>
2024-10-09 20:24:11 -0300

    Putting on code the first steps with tailwind in React

--- src/app/(main)/users/[username]/page.tsx
+++ src/app/(main)/users/[username]/page.tsx
@@ -1,6 +1,68 @@
+import Link from "next/link"
+
 const UserPage = ({params}: {params: {username: string}}) =>{
-    return <div>
-            Nombre de Usuario:{params.username} 
+    
+    const user = {
+        username: params.username,
+        name: 'John Doe',
+        bio: "Soy John Doe",
+        followersCount: 15,
+        followingCount: 3,
+        messages: [
+            {
+                message: 'Segundo mensaje',
+                repliesCount: 13,
+            },
+            {
+                message: 'Primer mensaje',
+                repliesCount: 13,
+            },
+        ],
+        replies: [
+            {
+                message: "Mi respuesta",
+                repliesCount: 0
+            },
+        ]
+    }
+
+    return <main className="flex flex-col bg-gray-100 p-8">
+        <section className="flex flex-col mb-8">
+            <div className="rounded-full p-6 bg-gray-300 w-20 text-center mb-4">
+                <span className="font-semibold text-lg">JD</span>
+            </div>
+            <h2 className="font-semibold text-lg mb-1">
+                {user.name}
+            </h2>
+            <div className="text-md mb-4 text-gray-600 cursor-pointer">
+                @<Link href={`/users/${user.username}`}>{user.username}</Link>
+            </div>
+            <div className="mb-4">
+                {user.bio}
+            </div>
+            <div className="flex justify-between">
+                <div>
+                <span className="font-semibold"> {user.followersCount}</span> Seguidores
+                </div>
+                <div>
+                <span className="font-semibold"> {user.followingCount} </span> Siguiendo
+                </div>
+            </div>
+        </section>
+        
+        <div className="flex justify-evenly mb-4">
+            <div className="cursor-pointer border-b-2 border-blue-400">
+                Mensajes
+            </div>
+            <div className="cursor-pointer">
+                Respuestas
+            </div>
         </div>
+        <div>
+            {user.messages.map((message, index) => 
+                <div key={`${index}`}>{message.message}</div>
+            )}
+        </div>
+    </main>
 }
 export default UserPage
